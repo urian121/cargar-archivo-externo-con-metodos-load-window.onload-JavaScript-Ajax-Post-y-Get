cc2ff4209798dfd06f6ddc42039a90200d6bab0d
urian <urian1213viera@gmail.com>
2022-07-20 10:24:09 -0500

    proyecto funcional

---
+++ archivo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Archivo</title>
+</head>
+<body>
+  <?php
+ echo  $_REQUEST['valor1'];
+ echo '<br>';
+ echo  $_REQUEST['valor2'];
+  ?>
+  <h2>Bienvenidos <strong>Developers...!</strong></h2>
+  
+</body>
+</html>
